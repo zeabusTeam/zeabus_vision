613c1a30a0a6d4bf0aeb3297daebf2383ed1c167
kam7411 <kam7411@git.com>
2019-05-24 20:30:31 +0700

    update path

--- src/path.py
+++ src/path.py
@@ -2,6 +2,7 @@
 import rospy
 import cv2 as cv
 import numpy as np
+import math
 from time import time
 from std_msgs.msg import Int64, Float64
 from sensor_msgs.msg import CompressedImage
@@ -52,19 +53,50 @@ def get_mask():
     upper = np.array([72, 255, 255], dtype=np.uint8)
     lower = np.array([28, 0, 0], dtype=np.uint8)
     mask = cv.inRange(image.hsv, lower, upper)
+    kernel = np.ones((5, 5), dtype=np.uint8)
+    mask = cv.GaussianBlur(mask, (5, 5), 0)
+    mask = cv.erode(mask, kernel)
+    mask = cv.erode(mask, kernel)
+    mask = cv.dilate(mask, kernel)
+    mask = cv.dilate(mask, kernel)
     return mask
 
 
 def get_obj(mask):
-    """
-        Filter some noise
-    """
-    return mask
-
+    himg, wimg = img.shape[:2]
+    cnt = cv.findContours(mask, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)[1]
+    for i in cnt :
+        if cv.contourArea(i) < 200 :
+            pass
+        else
+            x,y,w,h = cv2.boundingRect(i)
+            t_top = x  
+            b_top = x+(h/10)
+            t_mid = ((x+h)/2)-(h/10)
+            b_mid = ((x+h)/2)+(h/10)
+            t_bot = x+h-(h/10)
+            b_bot = x+h
+            c_top = cv.findContours(mask(cv.rectangle(t_top,y,b_top,h)), cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)[1]
+            c_middle = cv.findContours(mask(cv.rectangle(t_mid,y,b_mid,h)), cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)[1]
+            c_bottom = cv.findContours(mask(cv.rectangle(t_bot,y,b_bot,h)), cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)[1]
+            M_top = cv.moment(c_top)
+            M_bottom = cv.moment(c_middle)
+            M_bot = cv.moments(c_bottom)
+            cx_top = int(M_top["m10"]/M_top["m00"])
+            cy_top = int(M_top["m01"]/M_top["m00"])
+            cx_mid = int(M_mid["m10"]/M_mid["m00"])
+            cy_mid = int(M_mid["m01"]/M_mid["m00"])
+            cx_bot = int(M_bot["m10"]/M_bot["m00"])
+            cy_bot = int(M_bot["m01"]/M_bot["m00"])
+            rad_top = math.atan(abs(cy_top-cy_mid),abs(cx_top-cx_mid))
+            rad_bot = math.atan(abs(cy_mid-cy_bot),abs(cx_mid-cx_bot))
+            if (rad_bot >= 35 and rad_bot <= 55 and rad_top >= 35 and rad_bot <= 55) 
+                return cx_top , cy_top , cx_mid , cy_mid , cx_bot , cy_bot
+    return -1,-1,-1,-1,-1,-1 
 
 def find_path():
     mask = get_mask()
-    # obj = get_obj(mask)
+    obj = get_obj(mask)
     return message()
 
 
