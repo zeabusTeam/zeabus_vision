38f7cf2c8ae66118fd9b2f3ec662dbe65362bf46
kam7411 <kamkamkam7-4-11@hotmail.com>
2019-06-20 11:50:09 +0700

    update path

--- src/path_new.py
+++ src/path_new.py
@@ -4,7 +4,6 @@ import cv2 as cv
 import numpy as np
 import math
 from time import time
-from std_msgs.msg import Int64, Float64
 from sensor_msgs.msg import CompressedImage
 from zeabus_utility.msg import VisionPath
 from zeabus_utility.srv import VisionSrvPath
@@ -38,14 +37,14 @@ def mission_callback(msg):
         return find_path() 
 
 
-def message(cx1=0.0, cy1=0.0, cx2=0.0, cy2=0.0, cx3=0.0, cy3=0.0):
+def message(cx1=0.0, cy1=0.0, cx2=0.0, cy2=0.0, cx3=0.0, cy3=0.0, n=0 , area1 = 0.0 , area2 = 0.0):
     msg = VisionPath()
-    msg.cx1 = Float64(cx1)
-    msg.cy1 = Float64(cy1)
-    msg.cx2 = Float64(cx2)
-    msg.cy2 = Float64(cy2)
-    msg.cx3 = Float64(cx3)
-    msg.cy3 = Float64(cy3)
+    msg.point1 = tuple(cx1,cy1)
+    msg.point2 = tuple(cx2,cy2)
+    msg.point3 = tuple(cx3,cy3)
+    msg.n_point = int(n)
+    msg.area1 = float(area1)
+    msg.area2 = float(area2)
     return msg
 
 
@@ -82,35 +81,54 @@ def get_mask():
 
 
 def get_obj(mask):
-    # himg, wimg = img.shape[:2]
-    # cnt = cv.findContours(mask, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)[1]
-    # for i in cnt :
-    #     if cv.contourArea(i) < 200 :
-    #         pass
-    #     else :
-    #         x,y,w,h = cv2.boundingRect(i)
-    #         t_top = x  
-    #         b_top = x+(h/10)
-    #         t_mid = ((x+h)/2)-(h/10)
-    #         b_mid = ((x+h)/2)+(h/10)
-    #         t_bot = x+h-(h/10)
-    #         b_bot = x+h
-    #         c_top = cv.findContours(mask(cv.rectangle(t_top,y,b_top,h)), cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)[1]
-    #         c_middle = cv.findContours(mask(cv.rectangle(t_mid,y,b_mid,h)), cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)[1]
-    #         c_bottom = cv.findContours(mask(cv.rectangle(t_bot,y,b_bot,h)), cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)[1]
-    #         M_top = cv.moment(c_top)
-    #         M_bottom = cv.moment(c_middle)
-    #         M_bot = cv.moments(c_bottom)
-    #         cx_top = int(M_top["m10"]/M_top["m00"])
-    #         cy_top = int(M_top["m01"]/M_top["m00"])
-    #         cx_mid = int(M_mid["m10"]/M_mid["m00"])
-    #         cy_mid = int(M_mid["m01"]/M_mid["m00"])
-    #         cx_bot = int(M_bot["m10"]/M_bot["m00"])
-    #         cy_bot = int(M_bot["m01"]/M_bot["m00"])
-    #         rad_top = math.atan(abs(cy_top-cy_mid),abs(cx_top-cx_mid))
-    #         rad_bot = math.atan(abs(cy_mid-cy_bot),abs(cx_mid-cx_bot))
-    #         if (rad_bot >= 35 and rad_bot <= 55 and rad_top >= 35 and rad_bot <= 55) :
-    #             return cx_top , cy_top , cx_mid , cy_mid , cx_bot , cy_bot
+    himg, wimg = img.shape[:2]
+    cnt = cv.findContours(mask, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)[1]
+    for i in cnt :
+        if cv.contourArea(i) < 200 :
+            pass
+        else :
+            x,y,w,h = cv2.boundingRect(i)
+            t_top = y  
+            b_top = y+(h/10)
+            t_mid = ((y+h)/2)-(h/10)
+            b_mid = ((y+h)/2)+(h/10)
+            t_bot = y+h-(h/10)
+            b_bot = y+h
+            crop_t = mask[t_top:b_top,x:x+w]
+            crop_m = mask[t_mid:b_mid,x:x+w]
+            crop_b = mask[t_bot:b_bot,x:x+w]
+            c_top = cv.findContours(crop_t, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)[1]
+            c_middle = cv.findContours(crop_m, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)[1]
+            c_bottom = cv.findContours(crop_b, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)[1]
+            M_top = cv.moment(c_top)
+            M_bottom = cv.moment(c_middle)
+            M_bot = cv.moments(c_bottom)
+            cx_top = int(M_top["m10"]/M_top["m00"])
+            cy_top = int(M_top["m01"]/M_top["m00"])
+            cx_mid = int(M_mid["m10"]/M_mid["m00"])
+            cy_mid = int(M_mid["m01"]/M_mid["m00"])
+            cx_bot = int(M_bot["m10"]/M_bot["m00"])
+            cy_bot = int(M_bot["m01"]/M_bot["m00"])
+            if (h/w >= 1 and h/w <= 1.9) :
+                y_crop = y+(h/w)*100
+                crop_top = mask[y:y+h/2,x:x+w]
+                crop_bot = mask[y+h/2:y+h,w:x+w]
+                cnt_top = cv.findContours(crop_top, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)[1]
+                cnt_bot = cv.findContours(crop_bot, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)[1]
+                area1 = cv.contourArea(cnt_bot)/(himg*wimg)
+                area2 = cv.contourArea(cnt_top)/(himg*wimg)
+                if math.sqrt((((cx_top-cx_bot)**2) + ((cy_top-cy_bot)**2))) >= 45 and math.sqrt((((cx_top-cx_bot)**2) + ((cy_top-cy_bot)**2))) <= 49 :
+                    return cx1=cx_bot, cy1=cy_bot, cx2=cx_mid, cy2=cy_mid, cx3=cx_top, cy3=cy_bot, n=3 , area1 = area1 , area2 = area2
+            else :
+                if y == 0 and h <= himg/3:
+                    area1 = cv.contourArea()
+                    return cx1=cx_bot, cy1=cy_bot, cx2=cx_top, cy2=cy_top, cx3=0.0, cy3=0.0, n=2 , area1 = area1 , area2 = 0.0
+                    
+                    
+                # rect = cv.minAreaRect(cnt)
+                # box = cv.boxPoints(rect)
+                # box = np.int0(box)
+                
     return -1,-1,-1,-1,-1,-1 
 
 def find_path():
@@ -124,7 +142,7 @@ if __name__ == '__main__':
     output.log("INIT NODE", AnsiCode.GREEN)
     rospy.Subscriber('/bottom/left/image_raw/compressed', CompressedImage, image.callback)
     output.log("INIT SUBSCRIBER", AnsiCode.GREEN)
-    rospy.Service('vision/path',
+    rospy.Service('/vision/path',
                   VisionSrvPath(), mission_callback)
     output.log("INIT SERVICE", AnsiCode.GREEN)
     rospy.spin()
