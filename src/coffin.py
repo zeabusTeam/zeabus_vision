c27bb00b3b4a93cd76826bfb704df43a4b9e9888
AyumiizZ <Korrawitc@gmail.com>
2019-07-14 16:07:27 +0700

    :sparkles: finish box1

--- src/coffin.py
+++ src/coffin.py
@@ -6,13 +6,13 @@ import numpy as np
 from time import time
 from std_msgs.msg import Header
 from sensor_msgs.msg import CompressedImage
-from zeabus_utility.msg import VisionBox, VisionCoffins
-from zeabus_utility.srv import VisionSrvExposed
+from zeabus_utility.msg import VisionBox
+from zeabus_utility.srv import VisionSrvCoffin, VisionSrvCoffinResponse
 from constant import AnsiCode
 from vision_lib import OutputTools, ImageTools, TransformTools
 
 image = ImageTools(sub_sampling=0.5)
-output = OutputTools(topic='/vision/exposed')
+output = OutputTools(topic='/vision/coffin')
 transform = TransformTools()
 seq = 1
 
@@ -27,7 +27,7 @@ class Log:
 
 def mission_callback(msg):
     """
-        When call service it will run this 
+        When call service it will run this
         Returns:
             a group of process value from this program
     """
@@ -40,9 +40,8 @@ def mission_callback(msg):
         return find_coffin()
 
 
-def to_box(state=0, box=0, area=0.0):
+def to_box(box=0, area=0.0, color=(0, 255, 0), center=True):
     shape = image.display.shape[:2]
-    print('b', box)
     sort = sorted(box, key=lambda x: x[0])
     bottom = sort[:2]
     bottom = sorted(bottom, key=lambda x: x[1])
@@ -52,39 +51,45 @@ def to_box(state=0, box=0, area=0.0):
     top = sorted(top, key=lambda x: x[1])
     pt3 = top[0]
     pt1 = top[1]
-    box = VisionBox()
+    msg = VisionBox()
     for i in range(1, 5):
         print('pt'+str(i), tuple(eval('pt'+str(i))))
         cv.putText(image.display, str(i), tuple(eval('pt'+str(i))),
-                   cv.FONT_HERSHEY_SIMPLEX, 2, (0, 0, 255), 2, cv.LINE_AA)
-    box.point_1 = transform.convert_to_point(pt1, shape)
-    box.point_2 = transform.convert_to_point(pt2, shape)
-    box.point_3 = transform.convert_to_point(pt3, shape)
-    box.point_4 = transform.convert_to_point(pt4, shape)
-    box.area = transform.convert(area, shape[0]*shape[1])
-    return box
+                   cv.FONT_HERSHEY_SIMPLEX, 1, color, 2, cv.LINE_AA)
+    if center:
+        cx = cy = 0
+        for pt in box:
+            cx += pt[0]
+            cy += pt[1]
+        cx /= 4
+        cy /= 4
+        cv.putText(image.display, 'c', (cx, cy),
+                   cv.FONT_HERSHEY_SIMPLEX, 1, color, 2, cv.LINE_AA)
+        cv.circle(image.display, (cx, cy), 5, color, -1)
+        msg.point_1 = transform.convert_to_point((cx, cy), shape)
+        print('msg', msg)
+    else:
+        msg.point_1 = transform.convert_to_point(pt1, shape)
+        msg.point_2 = transform.convert_to_point(pt2, shape)
+        msg.point_3 = transform.convert_to_point(pt3, shape)
+        msg.point_4 = transform.convert_to_point(pt4, shape)
+    msg.area = transform.convert(area, shape[0]*shape[1])
+    return msg
 
 
-def message(box1=0, box2=0, state=0, name='coffin'):
-    global seq
+def message(box1=0, box2=0, state=0):
+    response = VisionSrvCoffinResponse()
     if state <= 0:
-        return VisionCoffins()
-    msg = VisionCoffins()
-    header = Header()
-    header.stamp = rospy.Time.now()
-    header.seq = seq
-    seq += 1
-    msg.header = header
-    msg.state = state
-    msg.name = name
+        return response
+    response.state = state
     if box2 == 0:
         print('a1', box1)
-        msg.data = [to_box(box=box1), VisionBox()]
+        response.data = [to_box(box=box1), VisionBox()]
     else:
-        msg.data = [to_box(box=box1), to_box(box=box2)]
+        response.data = [to_box(box=box1), to_box(box=box2)]
     output.publish(image.display, 'bgr', '/display')
-    print(msg)
-    return msg
+    print(response)
+    return response
 
 
 def get_mask():
@@ -120,23 +125,23 @@ def find_coffin():
         box2 = np.int64(box2)
         cv.drawContours(image.display, [box2], 0, (0, 0, 255), 2)
     print('len(cnt)', len(cnt))
-    if len(cnt) == 0:
+    if cnt == []:
         return message(state=0)
     elif len(cnt) == 1:
         return message(box1=box1, state=1)
-    elif len(cnt) == 2:
-        return message(box1=box1, box2=box2, state=1)
+    elif len(cnt) >= 2:
+        return message(box1=box1, box2=box2, state=2)
     return message(state=-2)
 
 
 if __name__ == '__main__':
-    rospy.init_node('vision_exposed', anonymous=False)
+    rospy.init_node('vision_coffin', anonymous=False)
     output.log("INIT NODE", AnsiCode.GREEN)
     rospy.Subscriber(image.topic('bottom'), CompressedImage,
                      image.callback, queue_size=1)
     output.log("INIT SUBSCRIBER", AnsiCode.GREEN)
-    rospy.Service('vision/exposed',
-                  VisionSrvExposed(), mission_callback)
+    rospy.Service('vision/coffin',
+                  VisionSrvCoffin(), mission_callback)
     output.log("INIT SERVICE", AnsiCode.GREEN)
     rospy.spin()
     output.log("END PROGRAM", AnsiCode.YELLOW_HL + AnsiCode.RED)
