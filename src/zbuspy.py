112b81d3fa4ed6c97601b8691364a9f66da07e7b
AyumiizZ <korrawitc@gmail.com>
2020-04-28 16:19:40 +0700

    :sparkles: add json and numpy

--- src/zbuspy.py
+++ src/zbuspy.py
@@ -15,6 +15,8 @@ import time
 class msgs:
     OPENCVIMAGE = 1
     JSON = 2
+    NUMPY_ARRAY = 3
+    PYDICT = 4
 
 
 class Publisher:
@@ -52,8 +54,11 @@ class Publisher:
         if self.datatype == msgs.OPENCVIMAGE:
             encoded, img_encoded = cv.imencode('.jpg', data)
             self.socket.send(img_encoded)
-        elif self.datatype == msgs.JSON:
-            self.socket.send(data)
+        elif self.datatype == msgs.JSON or msgs.PYDICT:
+            self.socket.send_json(data)
+        elif self.datatype == msgs.NUMPY_ARRAY:
+            self.socket.send_pyobj(data)
+            print('sent')
         else:
             print("Now support only json and opencv images")
 
@@ -79,7 +84,7 @@ class Subscriber:
         # self.socket.setsockopt(zmq.CONFLATE, 1) # use only last message
         self.socket.connect(self.connect_to)
         self.start_time = time.time()
-        self.image_count = 0
+        self.data_count = 0
         if self.enable_sync:
             self.sync()
         self.killed = False
@@ -99,23 +104,49 @@ class Subscriber:
 
     def subscribe(self, callback, callback_args=None):
         while not self.killed:
-            try:
-                data = self.socket.recv()
-                self.image_count += 1
-            except Exception:
-                print('Recieving End')
-                break
             if self.datatype == msgs.OPENCVIMAGE:
+                data = self.socket.recv()
+                self.data_count += 1
                 numpy_image = np.fromstring(data, dtype=np.uint8)
-                cv_image = cv.imdecode(numpy_image, cv.IMREAD_COLOR)
-                if callback_args is None:
-                    callback(cv_image)
-                else:
-                    callback(cv_image, callback_args)
+                data = cv.imdecode(numpy_image, cv.IMREAD_COLOR)
+            elif self.datatype == msgs.JSON or self.datatype == msgs.PYDICT:
+                data = self.socket.recv_json()
+                self.data_count += 1
+            elif self.datatype == msgs.NUMPY_ARRAY:
+                data = self.socket.recv_pyobj()
+                self.data_count += 1
+            else:
+                print("Not Support")
+                continue
+            if callback_args is None:
+                callback(data)
+            else:
+                callback(data, callback_args)
 
     def kill(self):
-        end_time = time() - self.start_time
-        print(1.0*self.image_count/end_time)
+        end_time = time.time()- self.start_time
+        print(1.0*self.data_count/end_time)
         self.killed = True
+        time.sleep(0.05)
         self.socket.close()
         self.ctx.term()
+
+class Core:
+    def __init__(self, port):
+        self.ip = '0.0.0.0'
+        self.port = port
+        self.core_ip = 'tcp://{}:{}'.format(self.ip, self.port)
+        self.ctx = zmq.Context()
+        self.socket = self.ctx.socket(zmq.REP)
+        self.socket.bind(self.bind_to)
+
+    def sync(self):
+        # use bind socket + 1
+        sync_with = 'tcp://{}:{}'.format(self.ip, int(self.port))
+        ctx = zmq.Context.instance()
+        s = ctx.socket(zmq.REP)
+        s.bind(sync_with)
+        print("Waiting for subscriber to connect...")
+        s.recv()
+        print("Sync Done.")
+        s.send_string('GO')
